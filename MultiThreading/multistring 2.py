4d195cc06c0e36e4614b079266bc6287a1e5a520
Payal Warhade <warhadepayal@gmail.com>
2022-10-04 00:02:17 +0530

    Add files via upload

---
+++ MultiThreading/multistring 2.py
@@ -0,0 +1,3 @@
+a="""RUN YOUR OWN RACE JUST TO IMPROVE NOT TO IMPRESS."""
+for x in a :
+    print (x,end=' ')
---
+++ MultiThreading/multistring 3.py
@@ -0,0 +1,5 @@
+s="RUN YOUR OWN RACE\
+JUST TO IMPROVE\
+NOT TO IMPRESS."
+for x in range (0,len(s)) :
+    print (s[x],end=' ')
